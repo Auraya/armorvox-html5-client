0f737a9cc231fe0dd465caf46159be26f11beed4
Wilson Zhang <wilson.zhang@auraya.net>
2018-08-21 14:31:42 +1000

    update default eval server url & vocab name

--- html/js/config.js
+++ html/js/config.js
@@ -26,9 +26,9 @@ const STEP_RETRY = "retry";
 const STEP_API_ERROR = "api_error";
 
 // api
-const SERVER = "https://cloud.armorvox.com/eval_server/vxml/v6/";
+const SERVER = "https://cloud.armorvox.com/evaluation/v6/";
 const GROUP = "your_group_name";
-const VOCAB = "en_us_v_2.0";
+const VOCAB = "en_digits";
 const QA_MODE = API_ENROL;
 const PN_MOBILE = "digit-mobile";
 const PN_RANDOM = "digit-random";
